6bae90ad0ede76785f6bf0e9ba1507b222fd4d44
Bob Sun <bob.sun@dtsanz.com>
2016-09-23 23:05:21 +1200

    Change chibi config

--- config/chibi-internal/config.js
+++ config/chibi-internal/config.js
@@ -14,7 +14,7 @@ module.exports = function () {
       host: 'http://www.cbxzfw.gov.cn/'
     },
     areaId: 421281,
-    host: 'http://10.10.10.4:12043',
+    host: 'http://10.10.10.4:9063',
     port: 3001,
     url: {
       bszn: {
